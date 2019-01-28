c1f85395a7d5eec735628e22127a6d1f7f07a1f9
sdim-ua <ingersol@yandex.ua>
2019-01-28 20:53:03 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,14 @@
+
+public class Cat {
+    //напишите тут ваш код
+    String name;
+    int age;
+    int weight;
+    int strength;
+    
+    
+
+    public static void main(String[] args) {
+
+    }
+}
